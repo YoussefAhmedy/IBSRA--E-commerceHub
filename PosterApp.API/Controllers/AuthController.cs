d3e62788a5c7f79943b35905ad2a2dc8ccc1c514
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-12 23:23:41 +0300

    Create AuthController.cs

---
+++ PosterApp.API/Controllers/AuthController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using PosterApp.Core.DTOs;
+using PosterApp.Core.Interfaces;
+
+namespace PosterApp.API.Controllers;
+
+public class AuthController : BaseController
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto request)
+    {
+        try
+        {
+            var response = await _authService.RegisterAsync(request);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return HandleException(ex);
+        }
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto request)
+    {
+        try
+        {
+            var response = await _authService.LoginAsync(request);
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return HandleException(ex);
+        }
+    }
+}
