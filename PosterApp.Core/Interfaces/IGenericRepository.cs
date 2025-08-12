d5069bf92d25902cb9bcf68ae192fcfa56fbf9b8
YoussefAhmedy <youssef.ahmedy189@gmail.com>
2025-08-12 22:56:23 +0300

    Create IGenericRepository.cs

---
+++ PosterApp.Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,11 @@
+namespace PosterApp.Core.Interfaces;
+
+public interface IGenericRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<bool> ExistsAsync(Guid id);
+}
